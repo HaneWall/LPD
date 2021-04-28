7e2c998fa2a57cd61f5625b1d193e3901de4c4d1
HaneWall <48854946+HaneWall@users.noreply.github.com>
2021-04-28 17:12:15 +0200

    added: DrudeMaterial slab variation gamma benchmark

--- dielectric_slab.py
+++ dielectric_slab.py
@@ -19,15 +19,15 @@ timesteps = 15000
 
 #number_density=5.86e28
 number_density = 1e27
-gamma = 6e13
+gammas = [0, 2e13, 9e13, 1.6e14]
 
 varying_d = np.arange(2, 200, 2)
-amplitudes = np.zeros(len(varying_d))
-phases = np.zeros(len(varying_d))
-theo_amplitudes = np.zeros(len(varying_d))
-theo_phases = np.zeros(len(varying_d))
+amplitudes = np.zeros((len(gammas),len(varying_d)))
+phases = np.zeros((len(gammas),len(varying_d)))
+theo_amplitudes = np.zeros((len(gammas),len(varying_d)))
+theo_phases = np.zeros((len(gammas),len(varying_d)))
 
-def run_setup(thickness_in_dz):
+def run_setup(thickness_in_dz, gamma):
     s_grid = LPD.Grid(Nz=Nz, dz=dz)
     s_grid[0] = LPD.LeftSideGridBoundary()
     s_grid[Nz-1] = LPD.RightSideGridBoundary()
@@ -43,46 +43,35 @@ def run_setup(thickness_in_dz):
     return s_grid.observers[0].amplitude, s_grid.observers[0].phase, theo_ampl, theo_phase
 
 
-def plot_results(varying_d, dz):
-    fig, axes = plt.subplots(2, figsize=(6, 5))
+def plot_results(gammas, varying_d, dz):
+    fig, axes = plt.subplots(figsize=(6, 5))
     plt.rcParams.update({
         "text.usetex": True,
         "font.family": "serif",
         "font.serif": ["Times"],
     })
     z = varying_d * dz
-    axes[0].tick_params(direction='in', bottom=True, left=True, right=True, top=True)
-    axes[0].grid(True, alpha=0.5, linestyle=':', color='k')
-    axes[0].plot(z, amplitudes, marker='o', color='k', mfc='None', ls='None', label=r'FDTD, $N_{\lambda}$='+'${}$'.format(N_lambda))
-    axes[0].plot(z, theo_amplitudes, color='red', label=r'Theory')
-    axes[0].set_xlabel('$z$ in m')
-    axes[0].set_ylabel('$E_{tr}$')
-    axes[0].legend(ncol=2, fancybox=False, edgecolor='k', bbox_to_anchor=(0, 1), loc=3)
-
-    axes[1].tick_params(direction='in', bottom=True, left=True, right=True, top=True)
-    axes[1].grid(True, alpha=0.5, linestyle=':', color='k')
-    axes[1].plot(z, phases, marker='o', color='k', mfc='None', ls='None')
-    axes[1].plot(z, theo_phases, color='red')
-    axes[1].set_xlabel('$z$ in m')
-    axes[1].set_ylabel('$\phi$')
-    plt.subplots_adjust(hspace=0)
+    axes.tick_params(direction='in', bottom=True, left=True, right=True, top=True)
+    axes.grid(True, alpha=0.5, linestyle=':', color='k')
+    for g_idx in range(len(gammas)):
+        axes.plot(z, theo_amplitudes[g_idx, :], label=r'Theory, $\gamma$=' + '${:.2E}$'.format(gammas[g_idx]))
+        if g_idx == 0:
+            axes.plot(z, amplitudes[g_idx, :], markersize=4, marker='o', color='k', mfc='None', ls='None', label=r'FDTD, $N_{\lambda}$='+'${}$'.format(N_lambda))
+        else:
+            axes.plot(z, amplitudes[g_idx, :], markersize=4, marker='o', color='k', mfc='None', ls='None')
+    axes.set_xlabel('$z$ in m')
+    axes.set_ylabel('$E_{tr}$')
+    axes.legend(ncol=1, fancybox=False, edgecolor='k', bbox_to_anchor=(0, 0.1), loc=3)
     plt.show()
 
-for idx, d in zip(range(len(varying_d)), varying_d):
-    amplitudes[idx], phases[idx], theo_amplitudes[idx], theo_phases[idx] = run_setup(d)
-    phases[idx] += np.angle(np.exp(-1j*k_0 * (obs_ind - src_ind) * dz))
-
-
-plot_results(varying_d, dz)
+'''for g_idx, gamma in zip(range(len(gammas)), gammas):
+    for d_idx, d in zip(range(len(varying_d)), varying_d):
+        amplitudes[g_idx, d_idx], phases[g_idx, d_idx], theo_amplitudes[g_idx, d_idx], theo_phases[g_idx, d_idx] = run_setup(d, gamma)
+        phases[g_idx, d_idx] += np.angle(np.exp(-1j*k_0 * (obs_ind - src_ind) * dz))
 '''
-grid = LPD.Grid(Nz=Nz, dz=dz)
-grid[src_ind] = LPD.Ramped_Sin(wavelength=800e-09, carrier_wavelength=10000e-09, amplitude=0.5, tfsf=True)
-grid[0] = LPD.LeftSideGridBoundary()
-grid[Nz-1] = LPD.RightSideGridBoundary()
-grid[4:20] = LPD.DrudeMaterial(name='testmaterial', eps_inf=1, number_density=5.86e27, gamma=8e12)
-#grid[60] = LPD.Point_Observer(name='test_obsever', first_timestep=1980)
-grid.run_timesteps(timesteps=timesteps, vis=True, ani=False)
 
-#print(grid.observers[0].amplitude, grid.observers[0].phase)
-#print(grid.observers[0].observedE)'''
+amplitudes = np.load(file='./LPD_FP3/saved_data/dielectric_slab/diel_slab_gamma_variation_fdtd.npy')
+theo_amplitudes = np.load(file='./LPD_FP3/saved_data/dielectric_slab/diel_slab_gamma_variation_theo.npy')
+
+plot_results(gammas, varying_d, dz)
 
